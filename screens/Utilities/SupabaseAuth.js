08f28fe84e17ceeca3df1bfe8ec8f50814f22028
Raman Prasad <60893770+rewatiramanprasad@users.noreply.github.com>
2023-10-02 17:30:05 +0000

    Register member connected with database

--- screens/Utilities/SupabaseAuth.js
+++ screens/Utilities/SupabaseAuth.js
@@ -1,12 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
+import { supabaseUrl, supabaseKey } from "../../env/credentials.json";
+// const SupabaseAuth = () => {
+// const supabaseUrl = "https://zyqpgpdsddwpfzfasjtc.supabase.co";
+// const supabaseKey =
+console.log(supabaseUrl);
+const supabase = createClient(supabaseUrl, supabaseKey);
+console.log(supabase);
+// return supabase;
+// };
 
-const SupabaseAuth = () => {
-  const supabaseUrl = "https://zyqpgpdsddwpfzfasjtc.supabase.co";
-  const supabaseKey =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5cXBncGRzZGR3cGZ6ZmFzanRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQzNjk2MDksImV4cCI6MjAwOTk0NTYwOX0.mpZR4-LLwwKKR_ONf_yNZU1dkWy9P_4VI3Cin8CMYQo";
-  const supabase = createClient(supabaseUrl, supabaseKey);
-
-  return supabase;
-};
-
-export default SupabaseAuth;
+export default supabase;
